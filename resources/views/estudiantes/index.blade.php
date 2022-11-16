32c77d075e7e5673f70203e39816ae76f359bad6
PedritoMatador <brujitoninamanco0509@gmail.com>
2022-11-16 11:16:23 -0500

    se agrg alumno

--- resources/views/estudiantes/index.blade.php
+++ resources/views/estudiantes/index.blade.php
@@ -9,7 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    <h2 class="text-center mb-5 p-5 text-lime-800 font-extrabold text-2xl shadow-md"> lista de estudiantes</h2>
+                    <h2 class="text-center mb-5 p-5 text-lime-800 font-extrabold text-2xl shadow-md"> lista de alumnos</h2>
                     <div>
                         <div class="overflow-x-auto relative">
                             <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
